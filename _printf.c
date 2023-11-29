03f35c892226f6b1cd5737c12e37c0b169d4b57a
Kamalusta <kamalusta@gmail.com>
2023-11-29 09:55:40 -0800

    betty style

--- _printf.c
+++ _printf.c
@@ -8,27 +8,17 @@
 int _printf(const char *format, ...)
 {
 int i = 0, j = 0, count = 0, count_fn = 0;
-format_t fuct[] = {
-{'c', print_char},
-{'s', print_string},
-{'%', print_percent},
-{'d', print_int},
-{'i', print_int},
-{'u', print_unsigned},
-{'o', print_octal},
-{'x', print_hex_lower},
-{'X', print_hex_upper},
-{'p', print_pointer},
-{'r', print_unknown},
-{'\0', NULL}
-};
+format_t fuct[] = {{'c', print_char}, {'s', print_string}, {'%', print_percent}
+, {'d', print_int}, {'i', print_int}, {'u', print_unsigned},
+{'o', print_octal}, {'x', print_hex_lower}, {'X', print_hex_upper},
+{'p', print_pointer}, {'r', print_unknown}, {'\0', NULL}};
 va_list ls;
 va_start(ls, format);
 while (format[i])
 {
 if (format[i] == '%')
 {
-if(strlen(format) == 1)
+if (strlen(format) == 1)
 return (-1);
 while (fuct[j].type)
 {
