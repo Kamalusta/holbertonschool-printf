b74ddb8630ec91c44851ca985bf250c2b2c479c5
Kamalusta <kamalusta@gmail.com>
2023-11-29 09:26:41 -0800

    adding print_string.c

--- _printf.c
+++ _printf.c
@@ -1,23 +1,5 @@
 #include "main.h"
 /**
- * print_string - printing for char
- * @lst: String to be copied
- *
- * Return: number of printed characters
- */
-int print_string(va_list lst)
-{
-char* w = va_arg(lst, char*);
-if (!w)
-{
-char* sw = "(null)";
-write(1,sw,strlen(sw));
-return (6);
-}
-else
-return (write(1,w,strlen(w)));
-}
-/**
  * _printf - printer function
  * @format: undifened number of arguments
  *
